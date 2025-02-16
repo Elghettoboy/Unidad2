ec793ee8b9b6220ac16d9419c302279afe92b5a9
Alessandro Sanchez <alessandrosanchez301214@gmail.com>
2025-02-15 18:55:03 -0600

    Codigos

---
+++ AlessandroSanchezPinto/Pares.java
@@ -0,0 +1,7 @@
+class Pares{
+  public static void main(String[] args) {
+  for(int i=0; i<=1000; i+=2){ //Se valua la condicion y se hace un incremento de 2 en 2.
+    System.out.println(i); //Asi imprime solo los pares.
+  }
+  }
+} 
