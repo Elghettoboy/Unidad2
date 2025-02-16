a53abfe184069e6feeed2908a0b1b14af3f3f9db
Alessandro Sanchez <alessandrosanchez301214@gmail.com>
2025-02-15 19:10:44 -0600

    Ejercicio2

---
+++ AlessandroSanchezPinto/Ejercicio2.java
@@ -0,0 +1,12 @@
+class Ejercicio2{
+  public static void main(String[] args) { //metodo principal
+   for(int num=0; num<=100; num++){ // hacemos la condicion con un for de 0 al 100, con un incremento de 1 en 1.
+
+     if (num %2==0) { // con if verifica si el numero es par.
+      System.out.println(num); //imprime solo los valores pares
+   }
+
+    
+   }
+  }
+  }
