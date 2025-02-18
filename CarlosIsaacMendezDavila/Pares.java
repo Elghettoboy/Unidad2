982e6a52cf7b8699eb9a096155714acf3de21492
IsaacMdzX <isaacmendeztp@gmail.com>
2025-02-17 21:51:18 -0600

    CIMD

---
+++ CarlosIsaacMendezDavila/Pares.java
@@ -0,0 +1,10 @@
+class Pares {
+ 
+   public static void main(String[] args) { // metodo principal
+        // ciclo for, que itera solo numeros pares
+        for (int i = 0; i <= 1000; i += 2) {
+            // impresion de los numeros
+            System.out.println("Numero Par: " + i);
+        }
+    }
+}
---
+++ CarlosIsaacMendezDavila/Tabla7.java
@@ -0,0 +1,16 @@
+public class Tabla7 {
+
+    public static void main(String[] args) {
+        for (int i = 7; i <= 7; i++) {
+            for (int j = 0; j <= 10; j++) {
+                System.out.println("7 x "+j+"= "+(i*j));
+
+            }
+        }
+    }
+}
+
+
+
+
+
