284deb81a00ed5d7b5961d9c96dfbb3ca125e65e
royxhry <rororoy1257@gmail.com>
2025-02-17 23:42:14 -0600

    q3

--- RodrigoArturoCruzTavera/TablaDel9.java
+++ RodrigoArturoCruzTavera/TablaDel9.java
@@ -1,9 +1,9 @@
-public class TablaDel7 {
+public class TablaDel9 {
 
     public static void main(String[] args) {
-        for (int i = 7; i <= 7; i++) {
+        for (int i = 9; i <= 9; i++) {
             for (int j = 0; j <= 10; j++) {
-                System.out.println("7 x "+j+"= "+(i*j));
+                System.out.println("9 x "+j+"= "+(i*j));
                 
             }
         }
